6adbce8ce907fa0493c40ce24d5841a59cab4c56
philogicae <38438271+philogicae@users.noreply.github.com>
2024-03-30 07:23:58 +0200

    Update gateway

--- ipfs_gateway/ipfs_gateway.py
+++ ipfs_gateway/ipfs_gateway.py
@@ -1,21 +1,19 @@
+from logging import basicConfig, getLogger, INFO
+from rich.logging import RichHandler
 from flask import Flask, abort, request, send_file
-from flask_script import Manager
 import os
 import requests
 import ipfshttpclient
-import logging
 
 IPFS_CONNECT_URL = "/ip4/127.0.0.1/tcp/5001/http"
 IPFS_FILE_URL = "http://127.0.0.1:8080/ipfs/"
 DOMAIN = "http://localhost:5000"
 
+basicConfig(
+    format="%(message)s", datefmt="[%d-%m %X]", level=INFO, handlers=[RichHandler()]
+)
+logger = getLogger("rich")
 app = Flask(__name__)
-
-manager = Manager(app)
-
-console = logging.StreamHandler()
-log = logging.getLogger("logger")
-log.addHandler(console)
 
 
 def download(url):
@@ -24,7 +22,9 @@ def download(url):
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        log.exception("IPFS Server Error! url:{0}, exception:{1}".format(url, str(e)))
+        logger.exception(
+            "IPFS Server Error! url:{0}, exception:{1}".format(url, str(e))
+        )
         return "IPFS Server Error! \n", 503
     if "content-type" in r.headers:
         return send_file(r.raw, r.headers["content-type"])
@@ -40,11 +40,11 @@ def down(path):
         hash = str(p[0])
         if not hash or not hash.startswith("Qm"):
             return "<Invalid Path>", 404
-        log.info("hash:{0}".format(hash), {"app": "dfile-down-req"})
+        logger.info("hash:{0}".format(hash), {"app": "dfile-down-req"})
         url = IPFS_FILE_URL + hash
         return download(url)
     except Exception as e:
-        log.exception("Download Error! path:{0}, exception:{1}".format(path, str(e)))
+        logger.exception("Download Error! path:{0}, exception:{1}".format(path, str(e)))
         return "Download Error! \n", 503
 
 
@@ -54,24 +54,17 @@ def up():
     try:
         if "file" in request.files:
             file = request.files["file"]
-            log.info("file name: {}".format(file.filename), {"app": "dfile-up-req"})
+            logger.info("file name: {}".format(file.filename), {"app": "dfile-up-req"})
             client = ipfshttpclient.connect(IPFS_CONNECT_URL)
             res = client.add(file)
-            log.info("upload res: {}".format(res), {"app": "dfile-up-res"})
+            logger.info("upload res: {}".format(res), {"app": "dfile-up-res"})
             url = DOMAIN + "/" + str(res["Hash"])
             return url
         abort(400)
     except Exception as e:
-        log.exception("Upload Error! exception:{}".format(str(e)))
+        logger.exception("Upload Error! exception:{}".format(str(e)))
         return "Upload Error! \n", 503
 
 
-@manager.command
-def debug():
-    app.run(debug=True, port=5000, host="0.0.0.0")
-    log.info("DFile is debugging.")
-
-
 if __name__ == "__main__":
-    manager.run()
-    log.info("DFile is running.")
+    app.run(host="0.0.0.0", port=5000)
--- ipfs_gateway/launch_ipfs_api.sh
+++ ipfs_gateway/launch_ipfs_api.sh
@@ -1,3 +1,3 @@
 apt install python3-full -y
-pip3 install -r requirements.txt  --break-system-packages 
-python3 ipfs_gateway.py
+pip3 update -r requirements.txt  --break-system-packages 
+gunicorn --access-logfile - -w 3 -b 0.0.0.0:2442 ipfs_gateway:app
