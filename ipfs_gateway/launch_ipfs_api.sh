1d7322a511b74993dedcb6c69a4ce85bcfd368f2
philogicae <38438271+philogicae@users.noreply.github.com>
2024-03-30 07:50:48 +0200

    Upgrade ipv6

--- ipfs_gateway/launch_ipfs_api.sh
+++ ipfs_gateway/launch_ipfs_api.sh
@@ -1,3 +1,3 @@
 apt install python3-full -y
 pip3 update -r requirements.txt  --break-system-packages 
-gunicorn --access-logfile - -w 2 -b 0.0.0.0:3456 ipfs_gateway:app
+gunicorn --access-logfile - -w 2 -b [::]:3456 ipfs_gateway:app
